717444a898c75e2581e8648bff103de3e63e1070
Frank Marra <frankrmarra@gmail.com>
2022-03-07 18:06:14 -0500

    added fontawesome icons as box choices

--- app.js
+++ app.js
@@ -4,33 +4,36 @@ const playAgain = document.querySelector('.play-again-button')
 
 //Main function of game
 const gameStart = (buttons) => {
+  //Reset if there was a previous game
   strikes.innerText = ''
   let pickOne = ''
   let pickTwo = ''
   let matchCount = 0
   playAgain.disabled = true
+  playAgain.innerText = 'Good Luck!'
   let gameArray = [
-    'one',
-    'one',
-    'two',
-    'two',
-    'three',
-    'three',
-    'four',
-    'four',
-    'five',
-    'five',
-    'six',
-    'six',
-    'seven',
-    'seven',
-    'eight',
-    'eight'
+    '<i class="fa-solid fa-music" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-music" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-compact-disc" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-compact-disc" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-headphones" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-headphones" style="font-size: 100px"></i>',
+    '<i class="fa-brands fa-spotify" style="font-size: 100px"></i>',
+    '<i class="fa-brands fa-spotify" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-guitar" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-guitar" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-play" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-play" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-drum" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-drum" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-microphone-lines" style="font-size: 100px"></i>',
+    '<i class="fa-solid fa-microphone-lines" style="font-size: 100px"></i>'
   ]
   let usedSymbol = []
+  //populate board with random boxes and add event listeners to each box
   buttons.forEach((box) => {
     box.disabled = false
-    box.innerText = ''
+    box.innerHTML = ''
     box.value = 0
     let randomSymbol = Math.floor(Math.random() * gameArray.length)
     //give HTML button a value.  Once button is clicked, this value will display in the inner text of the button.
@@ -40,26 +43,28 @@ const gameStart = (buttons) => {
     box.addEventListener('click', () => {
       if (pickOne === '') {
         pickOne = box
-        box.innerText = box.value
+        box.innerHTML = box.value
         pickOne.disabled = true
       } else if (pickOne !== '' && pickTwo === '') {
         pickTwo = box
-        box.innerText = box.value
+        box.innerHTML = box.value
         pickTwo.disabled = true
         if (pickOne.value !== pickTwo.value) {
           //Add a delay so player can see wrong choice
           const holdPlease = setTimeout(() => {
             strikes.innerText += 'X'
-            if (strikes.innerText === 'XXX') {
+            if (strikes.innerText === 'XXXX') {
               buttons.forEach((box) => (box.disabled = true))
+              buttons.forEach((box) => (box.innerHTML = ''))
               playAgain.disabled = false
-              pickOne.innerText = ''
-              pickTwo.innerText = ''
+              playAgain.innerText = 'Play Again?'
+              // pickOne.innerHTML = ''
+              // pickTwo.innerHTML = ''
             } else {
               pickOne.disabled = false
               pickTwo.disabled = false
-              pickOne.innerText = ''
-              pickTwo.innerText = ''
+              pickOne.innerHTML = ''
+              pickTwo.innerHTML = ''
               pickOne = ''
               pickTwo = ''
             }
@@ -70,6 +75,7 @@ const gameStart = (buttons) => {
           matchCount++
           if (matchCount === 8) {
             playAgain.disabled = false
+            playAgain.innerText = 'Play Again?'
           }
         }
       }
@@ -78,5 +84,3 @@ const gameStart = (buttons) => {
 }
 
 playAgain.addEventListener('click', () => gameStart(gameButtons))
-
-// gameStart(gameButtons)
