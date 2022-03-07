73cd99b40057358ee7e6cdd9dffc0871130860a8
Frank Marra <frankrmarra@gmail.com>
2022-03-07 15:24:31 -0500

    added a half second timer for wrong answers to display

--- app.js
+++ app.js
@@ -25,22 +25,33 @@ const gameStart = (gameButtons) => {
   let usedSymbol = []
   gameButtons.forEach((box) => {
     let randomSymbol = Math.floor(Math.random() * gameArray.length)
-    box.innerText = gameArray[randomSymbol]
+    // box.innerText = gameArray[randomSymbol]
     box.value = gameArray[randomSymbol]
     usedSymbol = gameArray.splice(randomSymbol, 1)
     box.addEventListener('click', () => {
       if (pickOne === '') {
         pickOne = box
+        box.innerText = box.value
         pickOne.disabled = true
       } else if (pickOne !== '' && pickTwo === '') {
         pickTwo = box
+        box.innerText = box.value
         pickTwo.disabled = true
-        console.log(pickOne, pickTwo)
         if (pickOne.value !== pickTwo.value) {
-          pickOne.disabled = false
-          pickTwo.disabled = false
-          pickOne = ''
-          pickTwo = ''
+          const holdPlease = setTimeout(() => {
+            pickOne.disabled = false
+            pickTwo.disabled = false
+            pickOne.innerText = ''
+            pickTwo.innerText = ''
+            pickOne = ''
+            pickTwo = ''
+          }, 500)
+          // pickOne.disabled = false
+          // pickTwo.disabled = false
+          // pickOne.innerText = ''
+          // pickTwo.innerText = ''
+          // pickOne = ''
+          // pickTwo = ''
         } else if (pickOne.value === pickTwo.value) {
           pickOne = ''
           pickTwo = ''
