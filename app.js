2fba116a30a86c8f4c115002a335c631c6cfe8c6
Frank Marra <frankrmarra@gmail.com>
2022-03-09 17:45:00 -0500

    removed search functionality and implemented label choice in its place.

--- app.js
+++ app.js
@@ -3,10 +3,114 @@ const strikes = document.querySelector('.strikes')
 const playAgain = document.querySelector('.play-again-button')
 const score = document.querySelector('.score-number')
 const strikeIcon = '<i class="fa-solid fa-xmark"></i>'
-const albums = JSON.parse(localStorage.getItem('albums'))
+// const albums = JSON.parse(localStorage.getItem('albums'))
 const albumArt = document.querySelector('.album-art')
 const albumTitle = document.querySelector('.album-title')
+//search page globals
+const SEARCH_DOMAIN = 'https://api.discogs.com/database/search?'
+const API_KEY = 'NFFuiOkGpBVELhAZbuku'
+const SECRET_KEY = 'vuxkHlTcqAaVwlHKbSkgDqSXILszAMeW'
+const searchButton = document.querySelector('.search-button')
+const genreName = document.getElementById('search-input')
+const searchResults = document.querySelector('.search-results')
+const rhymesayers = document.getElementById('rhymesayers')
+const sargentHouse = document.getElementById('sargent-house')
+const anti = document.getElementById('anti')
+const mysteryCircles = document.getElementById('mystery-circles')
+const desoto = document.getElementById('desoto')
+const merge = document.getElementById('merge')
+const epitaph = document.getElementById('epitaph')
+const dischord = document.getElementById('dischord')
+
+let albumArray = []
+
 let totalScore = 0
+
+//Search page function
+const albumArtList = (album) => {
+  album.forEach((alb) => {
+    let title = alb.title
+    let albumArt = alb.cover_image
+    let gameChoice = {
+      [title]: albumArt
+    }
+    albumArray.push(gameChoice)
+  })
+  console.log(albumArray)
+}
+
+//event listeners
+rhymesayers.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=13&per_page=20&label=rhymesayers&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+sargentHouse.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=3&per_page=20&label='sargent house'&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+anti.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=13&per_page=20&label=anti&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+mysteryCircles.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=3&per_page=20&label='mystery circles'&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+desoto.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=5&per_page=20&label=desoto&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+merge.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=9&per_page=20&label=merge&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+epitaph.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=10&per_page=20&label=epitaph&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
+
+dischord.addEventListener('click', async () => {
+  albumArray.length = 0
+  let response = await axios.get(
+    `${SEARCH_DOMAIN}page=4&per_page=20&label=dischord&key=${API_KEY}&secret=${SECRET_KEY}`
+  )
+  let album = response.data.results
+  albumArtList(album)
+})
 //Main function of game
 const gameStart = (buttons) => {
   //Reset if there was a previous game
@@ -111,8 +215,8 @@ playAgain.addEventListener('click', () => {
   if (albumArt.childNodes.length > 0) {
     albumArt.removeChild(albumArt.children[0])
   }
-  let randomNumber = Math.floor(Math.random() * albums.length)
-  let gameAlbum = albums[randomNumber]
+  let randomNumber = Math.floor(Math.random() * albumArray.length)
+  let gameAlbum = albumArray[randomNumber]
   let albumTitleName = Object.keys(gameAlbum).toString()
   let gameArt = Object.values(gameAlbum).toString()
   const imgMaker = document.createElement('img')
